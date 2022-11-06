538d30a2c36bfe61752b3a36721aab65d70d2378
Vitor <vitorcsharp@hotmail.com>
2022-11-06 14:21:34 -0300

    configuration ApplicationBingin done

--- lib/app/services/app_widget.dart
+++ lib/app/services/app_widget.dart
@@ -1,23 +1,32 @@
+import 'package:dwfifa/app/core/rest/custom_dio.dart';
 import 'package:dwfifa/app/core/theme/theme_config.dart';
 import 'package:dwfifa/app/pages/auth/login/login_page.dart';
 import 'package:dwfifa/app/pages/home/home_page.dart';
 import 'package:dwfifa/app/pages/splash/splash_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_getit/flutter_getit.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fifa World Cup Album',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeConfig.theme,
-      routes: {
-        '/': (_) => const SplashRouter(),
-        '/auth/login': (_) => const LoginPage(),
-        '/home': (_) => const HomePage(),
-      },
+    //(FlutterGetItApplicationBinding)ApplicationBinding => ele fica por cima de tudo na aplicação, e tudo que for colocado ali nunca morre, ou seja ele nunca da dispose.
+    return FlutterGetItApplicationBinding(
+      bindingsBuilder: () => [
+        //fica disponivel para toda a aplicação
+        Bind.lazySingleton((i) => CustomDio())
+      ],
+      child: MaterialApp(
+        title: 'Fifa World Cup Album',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeConfig.theme,
+        routes: {
+          '/': (_) => const SplashRouter(),
+          '/auth/login': (_) => const LoginPage(),
+          '/home': (_) => const HomePage(),
+        },
+      ),
     );
   }
 }
