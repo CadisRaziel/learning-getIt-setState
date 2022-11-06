258d043e9383b058a974709364d38a188954a6a2
Vitor <vitorcsharp@hotmail.com>
2022-11-06 14:13:36 -0300

    configuration env done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:dwfifa/app/core/config/env/env.dart';
 import 'package:dwfifa/app/services/app_widget.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  //antes de startar a aplicação ele le o que tem dentro do .env
+  await Env.instanceEnv.load();
   runApp(const AppWidget());
 }
